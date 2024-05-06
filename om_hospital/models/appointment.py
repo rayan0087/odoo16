4348df9b4e5dbfd030521a4f5544f9b4c0793409
rayan0087 <rayansarijatmiko@gmail.com>
2024-05-06 15:18:00 +0700

    (om_hospital) update vidio hingga ke 33

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -2,13 +2,36 @@ from odoo import models, fields, api
 
 
 class HospitalAppointment(models.Model):
-   _name = 'hospital.appointment'
-   _description = 'Hospital Appointment'
-   _inherit = ['mail.thread', 'mail.activity.mixin']
+   _name          = 'hospital.appointment'
+   _description   = 'Hospital Appointment'
+   _inherit       = ['mail.thread', 'mail.activity.mixin']
 
-   name = fields.Char(string='Appointment Name')
-   patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient')
-   appointment_time = fields.Datetime(string='Appointment Time')
-   booking_date = fields.Date(string='Booking Date')
+   name              = fields.Char(string='Appointment Name')
+   patient_id        = fields.Many2one(comodel_name='hospital.patient', string='Patient')
+   gender            = fields.Selection(string='Jenis Kelamin', related='patient_id.gender')
+   appointment_time  = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+   booking_date      = fields.Date(string='Booking Date', default=fields.Date.context_today)
+   ref               = fields.Char(string='Reference')
+   prescription      = fields.Html(string='Prescription')
+   priority = fields.Selection([
+      ('0', 'Normal'),
+      ('1', 'Low'),
+      ('2', 'High'),
+      ('3', 'Very High'),
+   ], string='priority')
+ 
+   state = fields.Selection([
+      ('draft', 'Draft'),
+      ('in_consultation', 'In Consultation'),
+      ('done', 'Done'),
+      ('cancel', 'Cancel'),
+   ], string='Status', default="draft", required=True)
    
+
+   @api.onchange('patient_id')
+   def _onchange_patient_id(self):
+      self.ref = self.patient_id.reference
+   
+   def action_test(self):
+      print('Button Clicked !!!!!!!')
    
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -6,15 +7,22 @@ class HospitalPatient(models.Model):
    _description   = 'Hospital Patient'
    _inherit       = ['mail.thread', 'mail.activity.mixin']
 
-   name       = fields.Char(string='Nama', tracking=True)
-   age        = fields.Integer(string='Umur', tracking=True)
-   reference  = fields.Char(string='Reference', tracking=True)
-   gender     = fields.Selection(string='Jenis Kelamin', selection=[('l', 'Laki Laki'), ('p', 'Perempuan')],  tracking=True)
+   name          = fields.Char(string='Nama', tracking=True)
+   age           = fields.Integer(string='Umur', compute='_compute_age',tracking=True)
+   date_of_birth = fields.Date(string='Date of Brith', tracking=True)
+   reference     = fields.Char(string='Reference', tracking=True, default='Rayans')
+   gender        = fields.Selection(string='Jenis Kelamin', selection=[('l', 'Laki Laki'), ('p', 'Perempuan')],  tracking=True, default='l')
    
    active     = fields.Boolean(string='Active', default=True)
 
-
-
+   @api.depends('date_of_birth')
+   def _compute_age(self):
+      for rec in self:
+         today = date.today()
+         if rec.date_of_birth:
+            rec.age = today.year - rec.date_of_birth.year
+         else:
+            rec.age = 0
 
 
 
